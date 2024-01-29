ef2d7040ef35d0f9fa55c317cc57f29b37459aef
idriss <idriss.said@epitech.eu>
2024-01-29 22:12:36 +0400

    Ajout numberOfRow en props

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,38 @@ import styles from './App.module.css';
 import { getData } from './getData';
 import { ImageData } from './types';
 
-const Data = () => {
-  const [ data ] = createResource<ImageData[]>(getData)
+interface DisplayDataProps {
+  numberOfRow: number
+}
+
+const DisplayData = (props: DisplayDataProps) => {
+  const [ data ] = createResource<ImageData[], number>(props.numberOfRow, getData)
   console.log(data)
   return (
-    <Show when={!data.loading} fallback={<>Tu fonctionnes maintenant ?</>}>
-      <For each={data()}>
-        {
-          (data) => (
-            <div style={{ width:"50%", height:"100%", display:"flex", "flex-direction":"row" }}>
-            <div style={{ width:"50%", height:"100%" }}>
-              <img src={data.url} style={{height: "100px", width: "100px"}}/>
-            </div>
-            <div style={{ width:"50%", height:"100%" }}>
-              <h1 style={{ color:"black" }}>{data.title}</h1>
-            </div>
-          </div>
-          )
-        }
-      </For>
-    </Show>
+      <div style={{ height:"40%", "overflow-x": "scroll", display:"flex", "flex-direction":"row" }}>
+        <Show when={!data.loading} fallback={<>Tu fonctionnes maintenant ?</>}>
+          <For each={data()}>
+            {
+              (data) => (
+              <div style={{ width:"600px", height:"100%", display:"flex", "flex-direction":"row" }}>
+                <img src={data.url} style={{ width: "300px" }}/>
+                <div style={{ width:"300px", height:"100%", display:"flex", "justify-content": "center", "align-items": "center" }}>
+                  <p style={{ color:"black" }}>{data.title}</p>
+                </div>
+              </div>
+              )
+            }
+          </For>
+        </Show>
+      </div>
   )
 }
 
 const App: Component = () => {
   return (
-    <div style={{ width:"100vw", height:"100vh" }}>
-      <div style={{ height:"50%", width:"100%", display:"flex", "flex-direction":"row" }}>
-        <Data/>
-        <Data/>
-      </div>
-      <div style={{ height:"50%", width:"100%", display:"flex", "flex-direction":"row" }}>
-        <Data/>
-        <Data/>
-      </div>
+    <div style={{ height:"100vh", display: "flex", "flex-direction": "column", "justify-content": "center", gap: "5em"}}>
+        <DisplayData numberOfRow={15}/>
+        <DisplayData numberOfRow={15}/>
     </div>
   );
 };
--- src/getData.ts
+++ src/getData.ts
@@ -1,9 +1,9 @@
 import { ImageData } from "./types"
 
-export async function getData() {
+export async function getData(numberOfRow: number) {
     const res = await fetch('https://jsonplaceholder.typicode.com/photos')
     const result = await res.json()
     const data = result as ImageData[]
     console.log(data)
-    return data
+    return data.slice(0, numberOfRow)
 }
