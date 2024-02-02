42e07977818f9d1ff4bcf998b04efa9a7f8380b2
idriss <idriss.said@epitech.eu>
2024-02-02 09:18:33 +0400

    Modification de l'affichage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { createResource, type Component, Show, For, createSignal } from 'solid-js';
+import { createResource, type Component, Show, For, createSignal, Setter } from 'solid-js';
 
 import { getData } from './getData';
 import { ImageData, getDataInterface } from './types';
 
 interface DisplayDataProps {
   numberOfRow: number
+  stop: boolean
 }
 
 const DisplayData = (props: DisplayDataProps) => {
   const [ inc, setInc ] = createSignal<number>(Math.floor(Math.random() * 50))
-  const [stop, setStop] = createSignal<boolean>(false)
   const [ data ] = createResource<ImageData[], getDataInterface>({start: () => inc(), end: props.numberOfRow} as getDataInterface, getData) || []
   const [dataSliced, setDataSliced] = createSignal<ImageData[] | undefined>(data()?.slice(inc(), props.numberOfRow + inc()))
 
   setInterval(() => {
-    if (stop()) return
+    if (props.stop) return
     if (inc() > 5000 - props.numberOfRow)
       setInc(0)
     else
@@ -25,23 +25,14 @@ const DisplayData = (props: DisplayDataProps) => {
   }, 1000)
 
   return (
-      <div class="h-1/3 overflow-x-scroll flex flex-row p-10 gap-3">
-        <div>
-          {
-            stop() ?
-            <button class='bg-red-500 rounded-lg p-2' style={{ width: "5%", height: "20%" }} onClick={() => setStop(!stop())}></button>
-            :
-            <button class='bg-green-500 rounded-lg p-2' style={{ width: "5%", height: "20%" }} onClick={() => setStop(!stop())}></button>
-          }
-          <h1>{inc()}</h1>
-        </div>
-        <Show when={!data.loading} fallback={<div class='flex w-full h-full justify-center items-center'>Chargement des données</div>}>
+      <div class="overflow-x-scroll flex flex-row gap-3 p-10">
+        <Show when={!data.loading} fallback={<div class='flex w-full h-full justify-center items-center'>Chargement des données...</div>}>
           <For each={dataSliced()}>
             {
-              (data) => (
-              <div class='flex flex-row shadow-xl'>
-                <img src={data.url}/>
-                <div class='flex justify-center items-center p-7' style={{ width:"600px", "max-width":"600px" }}>
+              (data, index) => (
+              <div id={index.toString()} class='flex flex-col shadow-xl bg-slate-300'>
+                <img src={data.url} style={{ width: "300px" }}/>
+                <div class='flex justify-center items-center p-7'>
                   <p class=' text-center'>{data.title}</p>
                 </div>
               </div>
@@ -53,11 +44,35 @@ const DisplayData = (props: DisplayDataProps) => {
   )
 }
 
-const App: Component = () => {
+function StopButton(props: { stop: boolean, setStop: Setter<boolean> }) {
   return (
-    <div class='h-screen flex flex-col justify-center gap-6'>
-        <DisplayData numberOfRow={10}/>
-        <DisplayData numberOfRow={10}/>
+    <div>
+    {
+      props.stop ?
+      <button class='bg-red-500 rounded-lg p-2' style={{ width: "5%", height: "5%" }} onClick={() => props.setStop(!props.stop)}></button>
+      :
+      <button class='bg-green-500 rounded-lg p-2' style={{ width: "5%", height: "5%" }} onClick={() => props.setStop(!props.stop)}></button>
+    }
+  </div>
+  )
+}
+
+function RandomImages(props: { height: string, width?: string | "auto", numberOfRow: number }) {
+  const [stop, setStop] = createSignal<boolean>(false)
+  return (
+    <div class='w-screen' style={{ height: props.height, width: props.width }}>
+      <StopButton stop={stop()} setStop={setStop}/>
+      <DisplayData stop={stop()} numberOfRow={props.numberOfRow}/>
+    </div>
+  )
+}
+
+const App: Component = () => {
+
+  return (
+    <div class='container flex flex-col justify-center gap-6'>
+      <RandomImages height='60vh' numberOfRow={10}/>
+      <RandomImages height='50vh' numberOfRow={10}/>
     </div>
   );
 };
